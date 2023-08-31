50ddefc2bfe47dc4cc8852abcd35396366f3e9a9
raghav-rama <hackerer528@gmail.com>
2023-08-31 13:35:30 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MintToken } from './components/MintToken';
+import SendSol from './components/WrapSOL';
 
 function App() {
   return (
@@ -8,16 +10,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <code>Mint Token</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <MintToken />
+       <SendSol />
       </header>
     </div>
   );
